7b243724a2323b72fb64b26844d8e6df16b018f5
Martin Pedersen <martin@m4r7.in>
2015-05-19 16:23:37 +0000

    Added lews

---
+++ bin.js
@@ -0,0 +1,77 @@
+#!/usr/bin/node
+var Lews = require('./lews'),
+    path = require('path'),
+    fs = require('fs');
+
+var lewsOptions = {
+    aetherDebugBarError: true,
+};
+var programOptions = {
+    srcPath: undefined,
+    dstPath: undefined,
+
+    useSocket: false,
+    watch: true
+};
+
+for (var i = 2; i < process.argv.length; i++) {
+    switch (process.argv[i].replace(/^--/, '')) {
+    case 'no-source-map':
+        lewsOptions.noSourceMap = true;
+        break;
+    case 'aether-debug-bar':
+        lewsOptions.aetherDebugBarError = program.argv[++i] != 'false';
+        break;
+    case 'debug':
+        lewsOptions.debug = true;
+        break;
+    case 'help':
+    case 'usage':
+        console.log([
+            'Usage: node ' + process.argv[1] + ' [options] sourcePath [destPath]',
+            '    --no-source-map: dont inline source maps',
+            '    --use-socket [socketPath]: listen on socket for files to compile (see README)',
+            '    --no-watch: don\'t watch for file changes (use this if filesystem is over NFS)',
+            '    --help: this help message'
+        ].join('\n\r'));
+        process.exit(1);
+        break;
+    case 'no-watch':
+        programOptions.watch = false;
+        break;
+    case 'use-socket':
+        try {
+            programOptions.useSocket = true;
+            var socketPath = path.resolve(process.argv[++i]);
+            programOptions.socketPath = socketPath;
+
+            var socketPathStats = fs.statSync(socketPath);
+            if (socketPathStats.isDirectory()) {
+                programOptions.socketPath = path.join(socketPath, 'lews.sock');
+            }
+        } catch (err) {
+        }
+        break;
+    default:
+        if (!programOptions.srcPath) {
+            programOptions.srcPath = path.resolve(process.argv[i]);
+        }
+        else if (!programOptions.dstPath) {
+            programOptions.dstPath = path.resolve(process.argv[i]);
+        }
+    }
+}
+
+
+if (!programOptions.srcPath) {
+    console.log('Missing source path. See --usage');
+    process.exit(1);
+}
+if (!programOptions.dstPath)
+    programOptions.dstPath = programOptions.srcPath;
+
+var lews = new Lews(programOptions.srcPath, programOptions.dstPath, lewsOptions);
+if (!programOptions.noWatch)
+    lews.watch();
+if (programOptions.useSocket)
+    lews.createSocket(programOptions.socketPath);
---
+++ lews.js
@@ -0,0 +1,249 @@
+/*
+ * Lews
+ *
+ * Compile changed less file and
+ * all the files that imports it.
+ * Inspired by Autoless.
+ *
+ * Author: Martin Pedersen
+ */
+
+var glob = require('glob'),
+    async = require('async'),
+    fs = require('fs'),
+    path = require('path'),
+    colors = require('colors'),
+    less = require('less');
+
+var Lews = function(srcPath, destPath, options) {
+    if (!options)
+        options = {};
+
+    this.srcPath = path.resolve(srcPath);
+    this.destPath = path.resolve(destPath);
+    this.debug = !!options.debug;
+
+    this.options = options || {
+        aetherDebugBarError: true
+    };
+
+    this.importMap = {};
+    this.lastModified = {};
+
+    var self = this;
+
+    // Create initial import map
+    glob(this.srcPath + '/**/*.less', function(err, files) {
+        async.eachLimit(
+            files,
+            4,
+            self.findImports.bind(self),
+            function(err) {
+                if(err) {
+                    console.error(err);
+                    process.exit(1);
+                }
+                if (self.debug)
+                    console.log(self.importMap);
+            }
+        );
+    });
+};
+
+var importRegex = /@import[^'";]*['"]([^'";]+)['"];/;
+var importRegexGlobal = new RegExp(importRegex.source, 'g');
+
+// Maybe use less.parse here?
+Lews.prototype.findImports = function(filename, cb) {
+    var self = this;
+    var relativeFilename = path.relative(self.srcPath, filename);
+    fs.readFile(filename, {encoding: 'utf8'}, function(err, data) {
+        if(err)
+            return cb(err);
+
+        var importMatch = data.match(importRegexGlobal);
+        if(!importMatch)
+            return cb();
+
+        for(var i = 0; i < importMatch.length; i++) {
+            var importFilename = importMatch[i].match(importRegex)[1];
+            var importFilepath = path.relative(self.srcPath, path.resolve(path.dirname(filename), importFilename));
+            self.importMap[importFilepath] = self.importMap[importFilepath] || [];
+            self.importMap[importFilepath].push(relativeFilename);
+        }
+
+        cb();
+    });
+};
+
+Lews.prototype.watch = function() {
+    var self = this;
+    /*
+    require('watch').createMonitor(this.srcPath, {ignoreDotFiles: true}, function(monitor) {
+        console.log('Monitoring files');
+        monitor.on('changed', function(filename) {
+            var relativeFilename = path.relative(self.srcPath, filename);
+            console.log('File changed', relativeFilename);
+
+            self.recompileFile(relativeFilename);
+        });
+    });
+   */
+    require('chokidar')
+    .watch(this.srcPath, {
+        ignored: /[\/\\]\./,
+        usePolling: true,
+        interval: 1000,
+        persistent: true
+    })
+    .on('ready', function() {
+        console.log('Monitoring files');
+    })
+    .on('change', function(filename) {
+        var relativeFilename = path.relative(self.srcPath, filename);
+        console.log('File changed', relativeFilename);
+        if (self.lastModified[relativeFilename] &&
+            Date.now() - self.lastModified[relativeFilename] < 1100)
+                return console.log(('Not recompiling ' + relativeFilename).yellow);
+
+        self.recompileFile(relativeFilename);
+    });
+};
+
+Lews.prototype.recompileFile = function(relativeFilename, callback) {
+    if (!callback)
+        callback = function() { console.log('Done with', relativeFilename); };
+
+    var self = this;
+
+    var files = [];
+    if(this.importMap[relativeFilename])
+        files = files.concat(this.importMap[relativeFilename]);
+
+    // Add files that imports added files recursively
+    for (var i = 0; i < files.length; i++)
+        if (this.importMap[files[i]])
+            for (var j = 0, file = this.importMap[files[i]][j]; j < this.importMap[files[i]].length; file = this.importMap[++j])
+                if (file && files.indexOf(file) === -1)
+                    files.push(file);
+
+    // Add current file if its less
+    if (relativeFilename.match(/\.less/))
+        files.unshift(relativeFilename);
+
+    if(!files.length)
+        return callback();
+
+    async.eachLimit(
+        files,
+        2,
+        function(lessFilename, cb) {
+            var inFile = path.join(self.srcPath, lessFilename),
+                outFile = path.join(self.destPath, lessFilename.replace(/\.less$/, '.css'));
+
+            fs.readFile(inFile, 'utf8', function(err, data) {
+                if (err)
+                    return cb(err);
+
+                var lessOptions = {
+                    paths: [path.dirname(inFile)],
+                    sourceMap: {
+                        sourceMapFileInline: !self.options.noSourceMap
+                    }
+                };
+
+                less.render(data, lessOptions, function(err, result) {
+                    console.log((lessFilename)[err ? 'red' : 'green']);
+                    if (err) {
+                        console.error('Error'.red, err.filename + ':', err.message);
+                        if (self.options.aetherDebugBarError) {
+                            self.lastModified[lessFilename] = Date.now();
+                            fs.writeFile(outFile, generateAetherDebugBarError(err), cb);
+                        }
+                        else
+                            cb(err);
+                        return;
+                    }
+
+                    // Update imports
+                    result.imports.forEach(function(importFilename) {
+                        var relativeImportFilename =
+                            path.relative(
+                                self.srcPath,
+                                path.resolve(path.dirname(inFile), importFilename)
+                            );
+
+                        if (!self.importMap[relativeImportFilename])
+                            self.importMap[relativeImportFilename] = [lessFilename];
+                        else if(self.importMap[relativeImportFilename].indexOf(lessFilename) === -1)
+                            self.importMap[relativeImportFilename].push(lessFilename);
+                    });
+
+                    fs.writeFile(outFile, result.css, function(err) {
+                        self.lastModified[lessFilename] = Date.now();
+                        if (err && err.code == 'ENOENT') // Directory does not exist
+                            fs.mkdir(path.dirname(outFile),  fs.writeFile.bind(this, outFile, result.css, cb));
+                        else
+                            cb(err);
+                    });
+                });
+            });
+        },
+        function(err) {
+            if (err)
+                console.error(err);
+
+            callback();
+        }
+    );
+};
+
+Lews.prototype.gulpWatchCb = function(event) {
+    if (event.type == 'deleted')
+        return;
+
+    var relativeFilename = path.relative(this.srcPath, event.path);
+    console.log('File changed', relativeFilename);
+    this.recompileFile(relativeFilename);
+};
+
+Lews.prototype.createSocket = function(socketPath) {
+    if (!socketPath)
+        socketPath = path.join(__dirname, 'lews.sock');
+
+    var self = this;
+    var net = require('net');
+    var server = net.createServer(function(conn) {
+        conn.setEncoding('utf8');
+        var body = '';
+        conn.on('data', function(data) {
+            body += data;
+        });
+        conn.on('end', function() {
+            console.log(body);
+            var relativePath = path.relative(self.srcPath, body);
+            if (relativePath.substr(0, 3) != '../')
+                self.recompileFile(relativePath);
+        });
+    });
+
+    server.listen(socketPath, function() {
+        console.log('Listening on socket', socketPath);
+    });
+
+    var unlinkFn = fs.unlinkSync.bind(fs, socketPath);
+    process
+    .on('exit', unlinkFn)
+    .on('SIGINT', process.exit);
+};
+
+function generateAetherDebugBarError(err) {
+    return [
+        '#aetherDebugBar { right: 0 !important; display: block !important; }',
+        '#aetherDebugBar div:before { content: "Error in ' +
+            path.basename(err.filename) + ':' + err.line +
+            '"; display: "block"; background: red; }'
+    ].join('\n');
+}
+
+module.exports = Lews;
